12024a42186bbbd368e6560325b12ac5ced05d45
Guzman Monne <guzmonne@hotmail.com>
2017-10-21 20:15:54 -0300

    added a user picture to the header

---
+++ src/components/Dashboard/Header/UserPicture/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+import React from 'react';
+import T from 'prop-types';
+
+const UserPicture = () => (
+  <a className="UserPicture">
+    <img src="http://themesdesign.in/admiry/blue/assets/images/users/avatar-1.jpg" alt="User"/>
+  </a>
+);
+
+export default UserPicture;
--- src/components/Dashboard/Header/index.js
+++ src/components/Dashboard/Header/index.js
@@ -1,10 +1,17 @@
 import './styles.css';
 import React from 'react';
+import T from 'prop-types';
+import UserPicture from './UserPicture/'
 
-const Header = () => (
+const Header = ({isAuthenticated}) => (
   <div className="Header">
     <h3>Meraki Quotes</h3>
+    {isAuthenticated === true && <UserPicture />}
   </div>
 );
+
+Header.propTypes = {
+  isAuthenticated: T.bool,
+};
 
 export default Header;
