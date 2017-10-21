0770a3074e56a1d9566ae14ba0836b5696a88dd4
Guzman Monne <guzmonne@hotmail.com>
2017-10-21 01:35:23 -0300

    update button component

--- src/components/Dashboard/Content/Login/Button/index.js
+++ src/components/Dashboard/Content/Login/Button/index.js
@@ -1,14 +1,22 @@
 import './styles.css';
 import React from 'react';
 import T from 'prop-types';
+import Spinner from './Spinner/';
 
-const Button = ({children, ...props}) => (
+const Button = ({loading, children, ...props}) => (
+  loading === true
+  ? 
+  <button className="btn" disabled>
+    <span><Spinner /></span>
+  </button>
+  : 
   <button className="btn" {...props}>
     <span>{children}</span>
   </button>
 );
 
 Button.propTypes = {
+  loading: T.bool,
   children: T.node,
 };
 
