331f47cafe754eac0fa67a1a66569960da0f7344
Guzman Monne <guzmonne@hotmail.com>
2017-10-22 10:18:59 -0300

    modified the ready epic to test the token validity every five seconds

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,9 +1,11 @@
 import {combineEpics} from 'redux-observable';
 import 'rxjs/add/observable/dom/ajax';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/concat';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/switchMap';
--- src/epics/ready.epic.js
+++ src/epics/ready.epic.js
@@ -12,36 +12,40 @@ export default action$ => (
   action$
   .ofType(VALIDATE_TOKEN)
   .switchMap(() => (
-    Observable.ajax.getJSON(
-      `${API_ROOT}/users/active`, {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${localStorage.getItem('TOKEN')}`
-    })
-    .concatMap(() => Observable.from([{
-      type: UPDATE_FLAGS,
-      payload: {
-        isAuthenticated: true,
-      }
-    }, {
-      type: LOGIN_SUCCESS,
-      payload: localStorage.getItem('TOKEN'),
-    }, {
-      type: UPDATE_FLAGS,
-      payload: {
-        ready: true,
-      }
-    }]))
-    .catch(error => (
-      Observable.from([{
+    Observable.interval(1000 * 60 * 5) // 5 minutes
+    .startWith(1)
+    .switchMap(() => (
+      Observable.ajax.getJSON(
+        `${API_ROOT}/users/active`, {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('TOKEN')}`
+      })
+      .concatMap(() => Observable.from([{
+        type: UPDATE_FLAGS,
+        payload: {
+          isAuthenticated: true,
+        }
+      }, {
+        type: LOGIN_SUCCESS,
+        payload: localStorage.getItem('TOKEN'),
+      }, {
         type: UPDATE_FLAGS,
         payload: {
           ready: true,
-          isAuthenticated: false,
         }
-      }, {
-        type: REDIRECT,
-        payload: '/'
-      }])
+      }]))
+      .catch(error => (
+        Observable.from([{
+          type: UPDATE_FLAGS,
+          payload: {
+            ready: true,
+            isAuthenticated: false,
+          }
+        }, {
+          type: REDIRECT,
+          payload: '/'
+        }])
+      ))
     ))
   ))
 );
