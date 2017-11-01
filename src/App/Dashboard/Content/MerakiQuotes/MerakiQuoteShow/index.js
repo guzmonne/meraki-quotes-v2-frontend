2d372425d17a97d7a03ae76a6cb13624544a770a
Guzman Monne <guzmonne@hotmail.com>
2017-11-01 17:39:06 -0300

    fetching the correct merakiQuote

--- src/App/Dashboard/Content/MerakiQuotes/MerakiQuoteShow/index.js
+++ src/App/Dashboard/Content/MerakiQuotes/MerakiQuoteShow/index.js
@@ -1,15 +1,25 @@
 import './styles.css';
 import React from 'react';
 import T from 'prop-types';
+import get from 'lodash/get';
 import MerakiQuoteShowHeader from './MerakiQuoteShowHeader/';
 import SearchMerakiDeviceForm from './SearchMerakiDeviceForm/';
 import MerakiQuoteShowGlobalOptions from './MerakiQuoteShowGlobalOptions/';
 import MerakiQuoteShowActionBar from './MerakiQuoteShowActionBar/';
 import {IMerakiQuotes, empty} from '../IMerakiQuotes.js';
+import Spinner from '../../../../../common/Spinner/';
 
 class MerakiQuoteShow extends React.Component {
+  componentWillMount() {
+    const key = get(this.props, 'match.params.key');
+    if (!key) return;
+    this.props.fetch(key);
+  }
+
   render() {
-    let {merakiQuote={}} = this.props;
+    let {merakiQuote={}, fetching} = this.props;
+
+    if (fetching === true) return <Spinner color="black" size="xl"/>
 
     return (
       <div className="MerakiQuoteShow">
@@ -24,6 +34,8 @@ class MerakiQuoteShow extends React.Component {
 
 MerakiQuoteShow.propTypes = {
   merakiQuote: T.shape(IMerakiQuotes),
+  fetching: T.bool,
+  fetch: T.func,
 };
 
 export default MerakiQuoteShow
--- src/App/Dashboard/Content/MerakiQuotes/MerakiQuoteShowContainer.js
+++ src/App/Dashboard/Content/MerakiQuotes/MerakiQuoteShowContainer.js
@@ -1,17 +1,29 @@
 import {connect} from 'react-redux';
-/*
 import get from 'lodash/get';
 import {
   API_SHOW,
-  UPDATE_UI,
-  DISPATCH_MULTIPLE_ACTIONS
 } from '../../../../store/actions.js';
-*/
+import {merakiQuotes} from '../../../../store/schemas.js';
 import MerakiQuoteShow from './MerakiQuoteShow/';
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state, props) => {
+  const key = get(props, 'match.params.key');
+  return {
+    merakiQuote: get(state, `entities.merakiQuotes.${key}`),
+    fetching: get(state, 'flags.merakiquotesApiShow'),
+  };
+};
 
-const mapActionsToProps = {};
+const mapActionsToProps = {
+  fetch: (key) => ({
+    type: API_SHOW,
+    payload: {
+      endpoint: `/merakiQuotes/${key}`,
+      target: 'merakiQuotes',
+      schema: merakiQuotes,
+    }
+  })
+};
 
 const MerakiQuoteShowContainer = (
   connect(mapStateToProps, mapActionsToProps)(MerakiQuoteShow)
